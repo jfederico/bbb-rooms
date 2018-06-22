53de836a5c5ae0a8657b2cc052762173721f329a
jfederico <jesus@123it.ca>
2018-06-22 15:58:25 +0000

    Turned join link into button

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   scope ENV['RELATIVE_URL_ROOT'] || '' do
     scope "rooms" do
       get "/launch", :to => "rooms#launch", as: :launch
-      get ":id/meeting/join", :to => "rooms#meeting_join", as: :meeting_join
-      get ":id/meeting/end", :to => "rooms#meeting_end", as: :meeting_end
+      post ":id/meeting/join", :to => "rooms#meeting_join", as: :meeting_join
+      post ":id/meeting/end", :to => "rooms#meeting_end", as: :meeting_end
       scope ":id/meetings" do
       end
     end
